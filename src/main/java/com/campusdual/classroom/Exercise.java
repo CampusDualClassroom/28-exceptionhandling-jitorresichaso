d4bcb195248fd979d3cb52c44b1d60357b934a5e
Juan Ignacio Torres Ichaso <jitorresichaso@macrotest.es>
2025-11-10 23:11:46 +0100

    Exercise 28 solved

--- src/main/java/com/campusdual/classroom/Exercise.java
+++ src/main/java/com/campusdual/classroom/Exercise.java
@@ -2,6 +2,9 @@ package com.campusdual.classroom;
 
 public class Exercise {
     public static void main(String[] args) {
-
+        try {
+            throw new Exception();
+        } catch (Exception e) {
+        }
     }
 }
